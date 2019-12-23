86b6213bf216cf1ea6cd9a24fa10c1c139325c96
pizzalll <58722972+pizzalll@users.noreply.github.com>
2019-12-23 10:53:04 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.slider').bxSlider({
+        mode: 'fade',
+        auto: true,
+        pager: true,
+        pause: 4000,
+    });
+  });
+
+$(document).ready(function(){
+    $('.bxslider').bxSlider({
+        auto: true,
+        pause: 6000,
+        minSlides: 1,
+        maxSlides: 3,
+        slideWidth: 300,
+        pager: false,
+        controls: false,
+        slideMargin: 150,
+        speed: 700,
+    });
+});
