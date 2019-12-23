807a37b27038e00a9953d4e7c99980b3006f5ea2
pizzalll <58722972+pizzalll@users.noreply.github.com>
2019-12-23 14:00:48 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,22 +1,47 @@
-$(document).ready(function(){
-    $('.slider').bxSlider({
-        mode: 'fade',
-        auto: true,
-        pager: true,
-        pause: 4000,
-    });
-  });
+// $(document).ready(function(){
+//     $('.slider').bxSlider({
+//         mode: 'fade',
+//         auto: true,
+//         pager: true,
+//         pause: 4000,
+//     });
+//   });
 
-$(document).ready(function(){
-    $('.bxslider').bxSlider({
-        auto: true,
-        pause: 6000,
-        minSlides: 1,
-        maxSlides: 3,
-        slideWidth: 300,
-        pager: false,
-        controls: false,
-        slideMargin: 150,
-        speed: 700,
+// $(document).ready(function(){
+//     $('.bxslider').bxSlider({
+//         auto: true,
+//         pause: 6000,
+//         minSlides: 1,
+//         maxSlides: 3,
+//         slideWidth: 300,
+//         pager: false,
+//         controls: false,
+//         slideMargin: 150,
+//         speed: 700,
+//     });
+// });
+
+$('body').imagesLoaded( function() {
+    $(document).ready(function(){
+        $('.slider').bxSlider({
+            mode: 'fade',
+            auto: true,
+            pager: true,
+            pause: 4000,
+        });
+      });
+    
+    $(document).ready(function(){
+        $('.bxslider').bxSlider({
+            auto: true,
+            pause: 6000,
+            minSlides: 1,
+            maxSlides: 3,
+            slideWidth: 300,
+            pager: false,
+            controls: false,
+            slideMargin: 150,
+            speed: 700,
+        });
     });
-});
+ });
